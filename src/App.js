7a67e4942257ebccf3a6c967a613ccb423808e87
Somers B Matthews <somersbmatthews@gmail.com>
2021-05-21 12:24:07 -0500

    added hoc stateless Aux component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+const App = () => {
   return (
     <div className="App">
 
---
+++ src/stateless/landingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Aux from 'Aux'
+
+const LandingPage = () => {
+    return(
+        <Aux>
+            
+        </Aux>
+    )
+}
