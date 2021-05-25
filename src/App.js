5afd94811296ef4e6d691dce142a2d83a3f12cda
Somers Matthews <somersbmatthews@gmail.com>
2021-05-25 12:50:55 -0500

    added Switch component to [App.js]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import logo from './logo.svg';
 import './App.css';
-const App = () => {
+
+function App() {
   return (
     <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <li>
+            <Link to="/">Home</Link>
+            </li>
 
+            <li>
+            <Link to="/portfolio"> </Link>
+            </li>
+            <li>
+            <Link to="/about"> </Link>  
+            </li>
+            <li>
+            <Link to="/contact"> </Link>
+            </li>
+          </nav>
+          <Switch>
+            <Route path="/">
+              <LandingPage />
+            </Route>
+            <Route>
+
+            </Route>
+            <Route>
+
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
